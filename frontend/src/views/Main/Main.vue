a693755cb810ea8d69e74dbf2310e61fcef729c9
Vadim Ignatov <ignatov0131@gmail.com>
2021-12-04 21:14:42 +0300

    Create main view

---
+++ frontend/src/views/Main/Main.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="main">
+    <div class="main__sidebar">
+      <navbar />
+    </div>
+    <div class="main__body">
+      <router-view />
+    </div>
+  </div>
+</template>
+
+<script>
+import Navbar from '@/components/Sidebar'
+
+export default {
+  name: 'Main',
+  components: {
+    Navbar
+  }
+}
+</script>
+
+<style lang="scss">
+@import "~@/styles/variables";
+
+.main {
+  display: flex;
+
+  &__sidebar {
+    max-width: $sidebarWidth;
+    height: 100vh;
+    position: sticky;
+    top: 0;
+    bottom: 0;
+  }
+
+  &__body {
+    flex-grow: 1;
+    padding: $gap-m;
+    overflow-y: scroll;
+  }
+}
+</style>
