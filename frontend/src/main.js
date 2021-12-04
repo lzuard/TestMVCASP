3c785e1b4bf508c3c95fb635465220deb8151f86
Vadim Ignatov <ignatov0131@gmail.com>
2021-12-04 15:24:17 +0300

    Link styles in main js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,6 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+
+/** Styles import */
 import 'bootstrap/dist/css/bootstrap.min.css'
+import '@/styles/global.scss'
 
 createApp(App).use(store).use(router).mount('#app')
