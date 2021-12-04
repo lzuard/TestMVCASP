a3d5a973c2cd27176825a2859221a1191bde59c0
Vadim Ignatov <ignatov0131@gmail.com>
2021-12-04 15:09:44 +0300

    Add auth route and meta title

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,10 +1,25 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import Auth from '@/views/Auth/Auth'
 
-const routes = []
+const routes = [
+  {
+    path: '/auth',
+    name: 'Authenticate',
+    component: Auth,
+    meta: {
+      title: 'Войти в систему'
+    }
+  }
+]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  document.title = `${to.meta.title ?? 'Складская система'} - WareHouse Store`
+  next()
 })
 
 export default router
