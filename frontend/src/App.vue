90e6f5d8873576f6df34e3a8839c7bb5ed692273
Vadim Ignatov <ignatov0131@gmail.com>
2021-12-04 15:45:44 +0300

    Replace all from app

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,15 +1,3 @@
 <template>
   <router-view/>
 </template>
-
-<style lang="scss">
-@import '~@/styles/variables';
-
-#app {
-  font-family: $primaryFont, $secondaryFont sans-serif;
-  background-color: $grey;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-}
-</style>
