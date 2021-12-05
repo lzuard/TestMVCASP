c406d5eb787cd1e4f424ece042863b5072e372aa
Vadim Ignatov <ignatov0131@gmail.com>
2021-12-05 17:52:38 +0300

    Add report cell

---
+++ frontend/src/components/Showcase/showcase-report-cell.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="col-12 showcase-report-cell">
+    <filterboard :title="title">
+      <slot />
+    </filterboard>
+  </div>
+</template>
+
+<script>
+import Filterboard from '@/components/Filterboard'
+
+export default {
+  name: 'showcase-report-cell',
+  components: {
+    Filterboard
+  },
+  props: {
+    title: {
+      type: String,
+      require: true
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.showcase-report-cell {
+  margin-bottom: 30px;
+}
+</style>
