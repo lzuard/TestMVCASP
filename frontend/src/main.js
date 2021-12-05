715ecae8d1facc1014e5d81db320dbe39f7b3bab
Vadim Ignatov <ignatov0131@gmail.com>
2021-12-05 15:09:04 +0300

    Register localization

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,9 +2,14 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import locale from './localization'
 
 /** Styles import */
 import 'bootstrap/dist/css/bootstrap.min.css'
 import '@/styles/global.scss'
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App)
+  .use(locale)
+  .use(store)
+  .use(router)
+  .mount('#app')
