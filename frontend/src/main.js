ac7c6e3a28eacacc82bef0515ac94c273597c6de
Vadim Ignatov <ignatov0131@gmail.com>
2021-12-05 16:17:24 +0300

    Create cover plugin

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,6 +4,9 @@ import router from './router'
 import store from './store'
 import locale from './localization'
 
+/** Plugins */
+import coverPlugin from './plugins/cover'
+
 /** Styles import */
 import 'bootstrap/dist/css/bootstrap.min.css'
 import '@/styles/global.scss'
@@ -12,4 +15,5 @@ createApp(App)
   .use(locale)
   .use(store)
   .use(router)
+  .use(coverPlugin)
   .mount('#app')
