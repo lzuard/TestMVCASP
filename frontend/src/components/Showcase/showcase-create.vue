2816dece0fdd12bbe4936717ffb1a104917dba3a
Vadim Ignatov <ignatov0131@gmail.com>
2021-12-05 11:18:35 +0300

    Add layout for creating entity

---
+++ frontend/src/components/Showcase/showcase-create.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="showcase-create">
+    <header class="showcase-create__header">
+      <showcase-title>
+        {{ title }}
+      </showcase-title>
+    </header>
+    <form @submit.prevent class="showcase-create__fields">
+      <div class="row">
+        <slot />
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+import ShowcaseTitle from './showcase-title'
+
+export default {
+  name: 'showcase-create',
+  components: {
+    ShowcaseTitle
+  },
+  props: {
+    title: {
+      type: String,
+      require: true
+    }
+  }
+}
+</script>
+
+<style>
+.showcase-create {
+  text-align: left;
+}
+</style>
