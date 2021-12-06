5d6ff0481b86eea2356991e88d86e88e6d8b833d
Izuard <sasha.20133@gmail.com>
2021-12-07 02:02:34 +0300

    Fill contract TTN

--- Services.Contracts/Contracts/TtnDto.cs
+++ Services.Contracts/Contracts/TtnDto.cs
@@ -4,5 +4,24 @@ namespace Contracts.Contracts
 {
     public class TtnDto: DtoBase
     {
+        /// <summary>
+        /// Возвращает или восстанавливает идентификатор (номер) ТТН.
+        /// </summary>
+        public string TtnID { get; set; }
+        
+        /// <summary>
+        /// Возвращает или восстанавливает тип ТТН
+        /// </summary>
+        public string Type { get; set; }
+        
+        /// <summary>
+        /// Возвращает или восстанавливает форму ТТН.
+        /// </summary>
+        public string Form { get; set; }
+        
+        /// <summary>
+        /// Возвращает или восстанавливает дату оформления.
+        /// </summary>
+        public string Date { get; set; } //TODO: date format
     }
 }
