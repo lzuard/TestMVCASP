23264d83abb1c3d23c9cd23d5c8983744c3b0add
Izuard <sasha.20133@gmail.com>
2021-12-07 01:43:34 +0300

    Fill contract ProductSupply

--- Services.Contracts/Contracts/ProductSupplyDto.cs
+++ Services.Contracts/Contracts/ProductSupplyDto.cs
@@ -4,6 +4,19 @@ namespace Contracts.Contracts
 {
     public class ProductSupplyDto : DtoBase
     {
+        /// <summary>
+        /// Восстанавливает или возвращает идентификатор поставки
+        /// </summary>
+        public int SupplyID { get; set; }
         
+        /// <summary>
+        /// Восстанавливает или возвращает идентификатор товара
+        /// </summary>
+        public int ProductID { get; set; }
+        
+        /// <summary>
+        /// Восстанавливает или возвращает количество товара
+        /// </summary>
+        public int Number { get; set; }
     }
 }
