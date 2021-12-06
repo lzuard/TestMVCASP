e0f2c8b0c5138e93b5b8849e2f310de787fa1a12
Izuard <sasha.20133@gmail.com>
2021-12-07 01:25:25 +0300

    Fill contract Order

--- Services.Contracts/Contracts/OrderDto.cs
+++ Services.Contracts/Contracts/OrderDto.cs
@@ -4,5 +4,76 @@ namespace Contracts.Contracts
 {
     public class OrderDto: DtoBase
     {
+        /// <summary>
+        /// Восстанавливает или возвращает идентификатор заказа
+        /// </summary>
+        public int OrderID { get; set; }
+        
+        /// <summary>
+        /// Восстанавливает или возвращает идентификатор клиента
+        /// </summary>
+        public int ClientID { get; set; }
+        
+        /// <summary>
+        /// Восстанавливает или возвращает идентификатор сотрудника
+        /// </summary>
+        public int EmployeeID { get; set; }
+        
+        /// <summary>
+        /// Восстанавливает или возвращает идентификатор ТК
+        /// </summary>
+        public int TransportCompanyID { get; set; }
+        
+        /// <summary>
+        /// Восстанавливает или возвращает идентификатор адреса
+        /// </summary>
+        public int AddressID { get; set; }
+        
+        /// <summary>
+        /// Восстанавливает или возвращает идентификатор ТТН
+        /// </summary>
+        public string TtnID { get; set; }
+        
+        /// <summary>
+        /// Восстанавливает или возвращает платежного документа
+        /// </summary>
+        public string PaymentDocument { get; set; }
+        
+        /// <summary>
+        /// Восстанавливает или возвращает дату заказа
+        /// </summary>
+        public string OrderingDate { get; set; } //TODO: date type
+        
+        /// <summary>
+        /// Восстанавливает или возвращает дату отгрузки
+        /// </summary>
+        public string ShipmentDate { get; set; } //TODO: date type
+        
+        /// <summary>
+        /// Восстанавливает или возвращает доставки
+        /// </summary>
+        public string DeliveryDate { get; set; } //TODO: date type
+        
+        /// <summary>
+        /// Восстанавливает или возвращает статус заказа
+        /// </summary>
+        public string Status { get; set; }
+        
+        /// <summary>
+        /// Восстанавливает или возвращает принято ли ТК
+        /// </summary>
+        public bool IsAcceptTransportCompany { get; set; }
+        
+        /// <summary>
+        /// Восстанавливает или возвращает принято ли клиентом
+        /// </summary>
+        public bool IsAcceptClient { get; set; }
+        
+        /// <summary>
+        /// Восстанавливает или возвращает примечание
+        /// </summary>
+        public string Extra { get; set; }
+        
+        
     }
 }
