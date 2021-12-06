eed80e3c346fb067fd8ddc0119bdaa277545cfb9
Izuard <sasha.20133@gmail.com>
2021-12-07 01:18:00 +0300

    Fill contract Employee

--- Services.Contracts/Contracts/EmployeeDto.cs
+++ Services.Contracts/Contracts/EmployeeDto.cs
@@ -4,5 +4,44 @@ namespace Contracts.Contracts
 {
     public class EmployeeDto: DtoBase
     {
+        /// <summary>
+        /// Восстанавливает или возвращает идентификатор сотрудника
+        /// </summary>
+        public int EmployeeID { get; set; }
+        
+        /// <summary>
+        /// Восстанавливает или возвращает фамилию 
+        /// </summary>
+        public string SecondName { get; set; }
+        
+        /// <summary>
+        /// Восстанавливает или возвращает имя
+        /// </summary>
+        public string FirstName { get; set; }
+        
+        /// <summary>
+        /// Восстанавливает или возвращает отчество
+        /// </summary>
+        public string ThirdName { get; set; }
+        
+        /// <summary>
+        /// Восстанавливает или возвращает дату рождения
+        /// </summary>
+        public string BirthDate { get; set; } //TODO: date type
+        
+        /// <summary>
+        /// Восстанавливает или возвращает номер телефона
+        /// </summary>
+        public string PhoneNumber { get; set; }
+        
+        /// <summary>
+        /// Восстанавливает или возвращает пароль
+        /// </summary>
+        public string Password { get; set; }
+        
+        /// <summary>
+        /// Восстанавливает или возвращает уволен ли сотрудник
+        /// </summary>
+        public bool IsFired { get; set; }
     }
 }
