08debbc21da584d6e43e398b5e1dd61808da4f1b
Izuard <sasha.20133@gmail.com>
2021-12-07 01:58:47 +0300

    Fill contract Supply

--- Services.Contracts/Contracts/SupplyDto.cs
+++ Services.Contracts/Contracts/SupplyDto.cs
@@ -4,5 +4,51 @@ namespace Contracts.Contracts
 {
     public class SupplyDto : DtoBase
     {
+        /// <summary>
+        /// Восстанавливает или возвращает идентификатор поставки
+        /// </summary>
+        public int SupplyID { get; set; }
+        
+        /// <summary>
+        /// Восстанавливает или возвращает идентификатор поставщика
+        /// </summary>
+        public int SupplierID { get; set; }
+        
+        /// <summary>
+        /// Восстанавливает или возвращает идентификатор сотрудника
+        /// </summary>
+        public int EmployeeID { get; set; }
+        
+        /// <summary>
+        /// Восстанавливает или возвращает идентификатор ТК
+        /// </summary>
+        public int TransportCompanyID { get; set; }
+
+        /// <summary>
+        /// Восстанавливает или возвращает идентификатор ТТН
+        /// </summary>
+        public string TtnID { get; set; }
+        
+        /// <summary>
+        /// Восстанавливает или возвращает платежного документа
+        /// </summary>
+        public string PaymentDocument { get; set; }
+        
+        /// <summary>
+        /// Восстанавливает или возвращает дату поставки
+        /// </summary>
+        public string SupplyDate { get; set; } //TODO: date type
+
+        /// <summary>
+        /// Восстанавливает или возвращает принято ли поставка
+        /// </summary>
+        public bool IsAccepted { get; set; }
+
+        /// <summary>
+        /// Восстанавливает или возвращает примечание
+        /// </summary>
+        public string Extra { get; set; }
+        
+        
     }
 }
