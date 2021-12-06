acd37f63c324f1ba246b5b5ca2bbd2b6332f53fb
Izuard <sasha.20133@gmail.com>
2021-12-07 01:52:30 +0300

    Fill contract Return

--- Services.Contracts/Contracts/ReturnDto.cs
+++ Services.Contracts/Contracts/ReturnDto.cs
@@ -4,5 +4,39 @@ namespace Contracts.Contracts
 {
     public class ReturnDto: DtoBase
     {
+        /// <summary>
+        /// Восстанавливает или возвращает идентификатор возврата
+        /// </summary>
+        public int ReturnID { get; set; }
+        
+        /// <summary>
+        /// Восстанавливает или возвращает идентификатор заказа
+        /// </summary>
+        public int OrderID { get; set; }
+        
+        /// <summary>
+        /// Восстанавливает или возвращает идентификатор ТТН
+        /// </summary>
+        public string TtnID { get; set; }
+        
+        /// <summary>
+        /// Восстанавливает или возвращает идентификатор Сотрудника
+        /// </summary>
+        public int EmployeeID { get; set; }
+        
+        /// <summary>
+        /// Восстанавливает или возвращает дату возврата
+        /// </summary>
+        public string ReturnDate { get; set; } //TODO: date type
+        
+        /// <summary>
+        /// Восстанавливает или возвращает акт возврата
+        /// </summary>
+        public string ReturnAct { get; set; }
+        
+        /// <summary>
+        /// Восстанавливает или возвращает идентификатор заказа
+        /// </summary>
+        public bool IsAccepted { get; set; }
     }
 }
