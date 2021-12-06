390d3253ab1b24a4daa8d4254aa355cf72601cc4
Izuard <sasha.20133@gmail.com>
2021-12-07 01:54:54 +0300

    Fill contract Supplier

--- Services.Contracts/Contracts/SupplierDto.cs
+++ Services.Contracts/Contracts/SupplierDto.cs
@@ -4,5 +4,39 @@ namespace Contracts.Contracts
 {
     public class SupplierDto: DtoBase
     {
+        /// <summary>
+        /// Возвращает или восстанавливает идентификатор поставщика.
+        /// </summary>
+        public int SupplierID { get; set; }
+
+        /// <summary>
+        /// Возвращает или восстанавливает идентификатор адреса.
+        /// </summary>
+        public int AddressId { get; set; }
+        
+        /// <summary>
+        /// Возвращает или восстанавливает ИНН клиента.
+        /// </summary>
+        public int IndividualTaxpayerNumber { get; set; }
+        
+        /// <summary>
+        /// Возвращает или восстанавливает имя организации клиента.
+        /// </summary>
+        public string OrganizationName { get; set; }
+        
+        /// <summary>
+        /// Возвращает или восстанавливает расчетный счет клиента.
+        /// </summary>
+        public string CheckingAccount { get; set; }
+        
+        /// <summary>
+        /// Возвращает или восстанавливает номер телефона клиента.
+        /// </summary>
+        public string Phone { get; set; }
+        
+        /// <summary>
+        /// Возвращает или восстанавливает электронную почту клиента.
+        /// </summary>
+        public string Email { get; set; }
     }
 }
