b67a0c5da61ae0011203c93cd9525192f3e56121
Izuard <sasha.20133@gmail.com>
2021-12-07 01:41:59 +0300

    Fill contract ProductOrder

--- Services.Contracts/Contracts/ProductOrderDto.cs
+++ Services.Contracts/Contracts/ProductOrderDto.cs
@@ -4,5 +4,19 @@ namespace Contracts.Contracts
 {
     public class ProductOrderDto: DtoBase
     {
+        /// <summary>
+        /// Восстанавливает или возвращает идентификатор заказа
+        /// </summary>
+        public int OrderID { get; set; }
+        
+        /// <summary>
+        /// Восстанавливает или возвращает идентификатор товара
+        /// </summary>
+        public int ProductID { get; set; }
+        
+        /// <summary>
+        /// Восстанавливает или возвращает количество товара
+        /// </summary>
+        public int Number { get; set; }
     }
 }
