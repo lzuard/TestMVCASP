785fd23219e6ff088df7c5c0368d30ba414be034
Izuard <sasha.20133@gmail.com>
2021-12-07 02:05:21 +0300

    Fill contract Utilization

--- Services.Contracts/Contracts/UtilizationDto.cs
+++ Services.Contracts/Contracts/UtilizationDto.cs
@@ -4,5 +4,34 @@ namespace Contracts.Contracts
 {
     public class UtilizationDto: DtoBase
     {
+        /// <summary>
+        /// Восстанавливает или возвращает идентификатор утилизации
+        /// </summary>
+        public int UtilizationID { get; set; }
+
+        /// <summary>
+        /// Восстанавливает или возвращает идентификатор сотрудника
+        /// </summary>
+        public int EmployeeID { get; set; }
+
+        /// <summary>
+        /// Восстанавливает или возвращает идентификатор ТТН
+        /// </summary>
+        public string TtnID { get; set; }
+        
+        /// <summary>
+        /// Восстанавливает или возвращает идентификатор утилизатора
+        /// </summary>
+        public int UtilizerID { get; set; }
+        
+        /// <summary>
+        /// Восстанавливает или возвращает акта утилизации
+        /// </summary>
+        public string UtilizationAct { get; set; }
+        
+        /// <summary>
+        /// Восстанавливает или возвращает дату отгрузки
+        /// </summary>
+        public string ShipmentDate { get; set; } //TODO: date type
     }
 }
