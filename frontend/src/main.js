fceaa594cf1c53bda71bb0003f23af2b6328fa38
Vadim Ignatov <ignatov0131@gmail.com>
2021-12-09 22:18:47 +0300

    Register toast plugin

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -7,11 +7,13 @@ import locale from './localization'
 /** Plugins */
 import coverPlugin from './plugins/cover'
 import apiPlugin from './plugins/api'
+import Toast from 'vue-toastification'
 
 /** Global components */
 import Preloader from '@/components/UI/preloader'
 
 /** Styles import */
+import 'vue-toastification/dist/index.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import '@/styles/global.scss'
 
@@ -21,5 +23,11 @@ createApp(App)
   .use(router)
   .use(coverPlugin)
   .use(apiPlugin)
+  .use(Toast, {
+    position: 'bottom-right',
+    transition: 'Vue-Toastification__fade',
+    pauseOnHover: false,
+    hideProgressBar: true
+  })
   .component('Preloader', Preloader)
   .mount('#app')
