0a246798703da0f31959d1ff298d839c534998c3
Vadim Ignatov <ignatov0131@gmail.com>
2021-12-09 23:45:15 +0300

    Upd create form

--- frontend/src/components/Showcase/showcase-create.vue
+++ frontend/src/components/Showcase/showcase-create.vue
@@ -7,24 +7,107 @@
     </header>
     <div class="showcase-create__fields">
       <div class="row">
-        <slot />
+
+        <showcase-label-field
+          v-for="(item, index) in fields"
+          :class="[
+             'col-md-6'
+          ]"
+          :key="index"
+          :label="item.label"
+        >
+          <input
+            v-if="item.type !== 'select'"
+            :class="[
+             'form-control'
+            ]"
+            :type="item.type ?? 'text'"
+            v-model="dataToSend.modelValue"
+            :data-bebra="dataToSend.modelValue"
+            :placeholder="item.placeholder"
+          />
+
+          <select
+            v-else
+            class="form-select"
+            v-model="dataToSend"
+          >
+            <option selected disabled>
+              {{ item.placeholder }}
+            </option>
+            <option
+              v-for="(selectItem, index) in item.values"
+              :key="index"
+              :value="selectItem.value"
+            >
+              {{ selectItem.text }}
+            </option>
+          </select>
+        </showcase-label-field>
+
+<!--        <showcase-errors-list-->
+<!--          v-if="v$.$errors.length"-->
+<!--          :errors="v$.$errors"-->
+<!--        />-->
+
+        <showcase-submit @click="onSend">
+          <edit-icon />
+          Создать
+        </showcase-submit>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import { useVuelidate } from '@vuelidate/core'
 import ShowcaseTitle from './showcase-title'
+import ShowcaseLabelField from './showcase-label-field'
+import ShowcaseSubmit from './showcase-submit-button'
+import { EditIcon } from '@iconicicons/vue3'
+// import { required } from '@/utils/validation/i18n-validators'
 
 export default {
   name: 'showcase-create',
   components: {
-    ShowcaseTitle
+    ShowcaseLabelField,
+    ShowcaseSubmit,
+    ShowcaseTitle,
+    EditIcon
+  },
+  setup () {
+    return { v$: useVuelidate() }
+  },
+  validations () {
+    return {
+      dataToSend: {}
+    }
   },
   props: {
     title: {
       type: String,
       require: true
+    },
+    fields: {
+      type: Array,
+      default: () => ([])
+    }
+  },
+  data: () => {
+    return {
+      dataToSend: {}
+    }
+  },
+  emits: ['onSend'],
+  methods: {
+    async onSend () {
+      const isReady = await this.v$.$validate()
+
+      if (!isReady) {
+        console.log('Ошибка!')
+      } else {
+        this.$emit('onSend', this.dataToSend)
+      }
     }
   }
 }
--- frontend/src/views/Main/TTN/TTNCreate.vue
+++ frontend/src/views/Main/TTN/TTNCreate.vue
@@ -1,123 +1,52 @@
 <template>
-  <showcase-create v-bind="createData">
-    <showcase-label class="col-md-6" label="Номер">
-      <input
-        class="form-control"
-        type="text"
-        v-model="dataToSend.number"
-        placeholder="Номер"
-      >
-    </showcase-label>
-
-    <showcase-label class="col-md-6" label="Дата оформления">
-      <input
-        class="form-control"
-        type="date"
-        v-model="dataToSend.issueDate"
-        placeholder="01.01.1990"
-      >
-    </showcase-label>
-
-    <showcase-label class="col-md-6" label="Тип ТТН">
-      <select class="form-select" v-model="dataToSend.ttnType">
-        <option disabled value="">
-          Тип ТТН
-        </option>
-        <option
-          v-for="(item, index) in ttnType"
-          :key="index"
-          :value="item.value"
-        >
-          {{ item.text }}
-        </option>
-      </select>
-    </showcase-label>
-
-    <showcase-label class="col-md-6" label="Форма ТТН">
-      <input
-        class="form-control"
-        type="text"
-        v-model="dataToSend.ttnForm"
-        placeholder="УТП-12"
-      >
-    </showcase-label>
-
-    <showcase-errors-list
-      v-if="v$.$errors.length"
-      :errors="v$.$errors"
-    />
-
-    <showcase-submit @click="sendToServer">
-      <edit-icon />
-      Создать
-    </showcase-submit>
-  </showcase-create>
+  <showcase-create v-bind="createData" />
 </template>
 
 <script>
-import { useVuelidate } from '@vuelidate/core'
-import ShowcaseErrorsList from '@/components/Showcase/showcase-errors-list'
 import ShowcaseCreate from '@/components/Showcase/showcase-create'
-import ShowcaseLabel from '@/components/Showcase/showcase-label-field'
-import ShowcaseSubmit from '@/components/Showcase/showcase-submit-button'
-import { EditIcon } from '@iconicicons/vue3'
-import {
-  required
-} from '@/utils/validation/i18n-validators'
 
 export default {
   name: 'AgentCreate',
   components: {
-    ShowcaseCreate,
-    ShowcaseLabel,
-    ShowcaseSubmit,
-    ShowcaseErrorsList,
-    EditIcon
-  },
-  setup () {
-    return { v$: useVuelidate() }
-  },
-  validations () {
-    return {
-      dataToSend: {
-        number: { required },
-        issueDate: { required },
-        ttnType: { required },
-        ttnForm: { required }
-      }
-    }
+    ShowcaseCreate
   },
   data: () => {
     return {
       createData: {
-        title: 'Создать ТТН'
-      },
-      ttnType: [
-        {
-          text: 'Расход',
-          value: 'minus'
-        },
-        {
-          text: 'Приход',
-          value: 'plus'
-        }
-      ],
-      dataToSend: {
-        number: '',
-        issueDate: '',
-        ttnType: '',
-        ttnForm: ''
-      }
-    }
-  },
-  methods: {
-    async sendToServer () {
-      const isReady = await this.v$.$validate()
-
-      if (!isReady) {
-        console.log('Ошибка!')
-      } else {
-        console.log('Объект готов к отправке', this.dataToSend)
+        title: 'Создать ТТН',
+        fields: [
+          {
+            label: 'Номер',
+            placeholder: 'Номер',
+            modelValue: 'number'
+          },
+          {
+            label: 'Дата оформления',
+            placeholder: '01.01.1990',
+            type: 'date',
+            modelValue: 'issueDate'
+          },
+          {
+            label: 'Тип ТТН',
+            placeholder: 'Тип ТТН',
+            type: 'select',
+            values: [
+              {
+                text: 'Расход',
+                value: 'minus'
+              },
+              {
+                text: 'Приход',
+                value: 'plus'
+              }
+            ]
+          },
+          {
+            label: 'Форма ТТН',
+            placeholder: 'УТП-12',
+            modelValue: 'ttnForm'
+          }
+        ]
       }
     }
   }
