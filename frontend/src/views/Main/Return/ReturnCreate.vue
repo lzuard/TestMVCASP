57cfda38a67a35b239fbecf1a28f428172b78369
Vadim Ignatov <ignatov0131@gmail.com>
2021-12-10 23:02:28 +0300

    Rework return create

--- frontend/src/views/Main/Return/ReturnCreate.vue
+++ frontend/src/views/Main/Return/ReturnCreate.vue
@@ -1,68 +1,9 @@
 <template>
-  <showcase-create v-bind="createData">
-    <showcase-label class="col-md-6" label="Номер ТТН заказа">
-      <input
-        class="form-control"
-        type="text"
-        v-model="dataToSend.ttnOrder"
-        placeholder="Номер ТТН заказа"
-      >
-    </showcase-label>
-
-    <showcase-label class="col-md-6" label="Дата возврата">
-      <input
-        class="form-control"
-        type="date"
-        v-model="dataToSend.returnDate"
-        placeholder="01.01.1990"
-      >
-    </showcase-label>
-
-    <showcase-label class="col-md-6" label="Номер ТТН возврата">
-      <input
-        class="form-control"
-        type="text"
-        v-model="dataToSend.ttnReturn"
-        placeholder="Номер ТТН возврата"
-      >
-    </showcase-label>
-
-    <showcase-label class="col-md-6" label="Номер акта возврата">
-      <input
-        class="form-control"
-        type="text"
-        v-model="dataToSend.actOfReturn"
-        placeholder="Номер акта возврата"
-      >
-    </showcase-label>
-
-    <showcase-label class="col-12" label="Принято">
-      <input
-        class="form-check-input"
-        type="checkbox"
-        v-model="dataToSend.accept"
-      >
-    </showcase-label>
-
-    <showcase-errors-list
-      v-if="v$.$errors.length"
-      :errors="v$.$errors"
-    />
-
-    <showcase-submit @click="sendToServer">
-      <edit-icon />
-      Создать
-    </showcase-submit>
-  </showcase-create>
+  <showcase-create v-bind="createData" @onSend="sendToServer" />
 </template>
 
 <script>
-import { useVuelidate } from '@vuelidate/core'
-import ShowcaseErrorsList from '@/components/Showcase/showcase-errors-list'
 import ShowcaseCreate from '@/components/Showcase/showcase-create'
-import ShowcaseLabel from '@/components/Showcase/showcase-label-field'
-import ShowcaseSubmit from '@/components/Showcase/showcase-submit-button'
-import { EditIcon } from '@iconicicons/vue3'
 import {
   required
 } from '@/utils/validation/i18n-validators'
@@ -70,49 +11,51 @@ import {
 export default {
   name: 'ReturnCreate',
   components: {
-    ShowcaseCreate,
-    ShowcaseLabel,
-    ShowcaseSubmit,
-    ShowcaseErrorsList,
-    EditIcon
-  },
-  setup () {
-    return { v$: useVuelidate() }
-  },
-  validations () {
-    return {
-      dataToSend: {
-        ttnOrder: { required },
-        returnDate: { required },
-        ttnReturn: { required },
-        actOfReturn: { required }
-      }
-    }
+    ShowcaseCreate
   },
   data: () => {
     return {
       createData: {
-        title: 'Создать возврат'
-      },
-      dataToSend: {
-        ttnOrder: '',
-        returnDate: '',
-        ttnReturn: '',
-        actOfReturn: '',
-        accept: false
+        title: 'Создать возврат',
+        fields: [
+          {
+            label: 'Номер ТТН заказа',
+            placeholder: 'ТТН',
+            modelValue: 'ttnOrder',
+            validation: { required }
+          },
+          {
+            label: 'Дата возврата',
+            placeholder: 'Дата возврата',
+            type: 'date',
+            modelValue: 'returnDate',
+            validation: { required }
+          },
+          {
+            label: 'Номер ТТН возврата',
+            placeholder: 'ТТН',
+            modelValue: 'ttnReturn',
+            validation: { required }
+          },
+          {
+            label: 'Номер акта возврата',
+            placeholder: 'Акт',
+            modelValue: 'actOfReturn',
+            validation: { required }
+          },
+          {
+            label: 'Принято',
+            modelValue: 'accept',
+            type: 'checkbox',
+            cols: ['col-auto'],
+            validation: { required }
+          }
+        ]
       }
     }
   },
   methods: {
-    async sendToServer () {
-      const isReady = await this.v$.$validate()
-
-      if (!isReady) {
-        console.log('Ошибка!')
-      } else {
-        console.log('Объект готов к отправке', this.dataToSend)
-      }
-    }
+    async sendToServer () {}
   }
 }
 </script>
