a46f71adeb88faade129f709ebbb275707ba56e3
Vadim Ignatov <ignatov0131@gmail.com>
2021-12-10 22:47:51 +0300

    Create textarea custom component

---
+++ frontend/src/components/UI/textareaField.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="textarea-field">
+    <textarea
+      :class="[
+        'form-control',
+        {
+          'is-invalid': isError
+        }
+      ]"
+      :value="modelValue"
+      @input="$emit('update:modelValue', $event.target.value)"
+    >
+    </textarea>
+
+    <span
+      v-if="isError"
+      class="input-error-caption input-error-caption--textarea"
+    >
+      {{ errorText }}
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'textareaField',
+  emits: ['update:modelValue'],
+  props: {
+    placeholder: {
+      type: String,
+      default: ''
+    },
+    modelValue: {
+      type: String,
+      default: ''
+    },
+    isError: {
+      type: Boolean,
+      default: false
+    },
+    errorText: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+
+<style>
+.textarea-field {
+  position: relative;
+  resize: none;
+}
+</style>
