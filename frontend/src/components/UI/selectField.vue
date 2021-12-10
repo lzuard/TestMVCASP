11206fe1f0aa06f8b94ee7532e4ca712906e56c7
Vadim Ignatov <ignatov0131@gmail.com>
2021-12-10 15:03:18 +0300

    Create select field

---
+++ frontend/src/components/UI/selectField.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="select-field">
+    <select
+      :class="[
+        'form-select',
+        {
+          'is-invalid': isError
+        }
+      ]"
+      :value="modelValue"
+      @change="$emit('update:modelValue', $event.target.value)"
+    >
+      <option selected disabled>
+        {{ placeholder }}
+      </option>
+
+      <option
+        v-for="(selectItem, index) in values"
+        :key="index"
+        :value="selectItem.value"
+      >
+        {{ selectItem.text }}
+      </option>
+    </select>
+
+    <span
+      v-if="isError"
+      class="input-error-caption input-error-caption--select"
+    >
+      {{ errorText }}
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'selectField',
+  emits: ['update:modelValue'],
+  props: {
+    placeholder: {
+      type: String,
+      default: ''
+    },
+    values: {
+      type: Array,
+      required: true
+    },
+    modelValue: {
+      type: String,
+      default: ''
+    },
+    isError: {
+      type: Boolean,
+      default: false
+    },
+    errorText: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+
+<style>
+.select-field {
+  position: relative;
+}
+</style>
