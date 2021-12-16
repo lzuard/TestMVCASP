808a1f3b97247f5522e1ee30cad8af9b69f2aad4
Vadim Ignatov <ignatov0131@gmail.com>
2021-12-16 19:21:50 +0300

    Create showcase header

---
+++ frontend/src/components/Showcase/showcase-header.vue
@@ -0,0 +1,47 @@
+<template>
+  <header class="showcase-header">
+    <showcase-title>
+      {{ title }}
+    </showcase-title>
+    <router-link class="link showcase-header__create" :to="linkToCreate">
+      <file-plus-icon />
+      {{ createCaption }}
+    </router-link>
+  </header>
+</template>
+
+<script>
+import ShowcaseTitle from './showcase-title'
+import { FilePlusIcon } from '@iconicicons/vue3'
+
+export default {
+  name: 'showcase-header',
+  components: {
+    ShowcaseTitle,
+    FilePlusIcon
+  },
+  props: {
+    title: {
+      type: String,
+      require: true
+    },
+    linkToCreate: {
+      type: String,
+      require: true
+    },
+    createCaption: {
+      type: String,
+      require: true
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.showcase-header {
+  &__create {
+     font-weight: 600;
+     display: inline-flex;
+   }
+}
+</style>
