ca30b9b0b6648228aa2d075179971164dc10fa36
Vadim Ignatov <ignatov0131@gmail.com>
2021-12-17 22:16:32 +0300

    Rework order create

--- frontend/src/views/Main/Order/OrderCreate.vue
+++ frontend/src/views/Main/Order/OrderCreate.vue
@@ -1,255 +1,140 @@
 <template>
-  <showcase-create v-bind="createData">
-    <showcase-label class="col-md-6" label="Клиент">
-      <select class="form-select" v-model="dataToSend.client">
-        <option disabled value="">
-          Клиент
-        </option>
-        <option
-          v-for="(item, index) in clientList"
-          :key="index"
-          :value="item.value"
-        >
-          {{ item.text }}
-        </option>
-      </select>
-    </showcase-label>
-
-    <showcase-label class="col-md-6" label="ТК">
-      <select class="form-select" v-model="dataToSend.tc">
-        <option disabled value="">
-          ТК
-        </option>
-        <option
-          v-for="(item, index) in tcList"
-          :key="index"
-          :value="item.value"
-        >
-          {{ item.text }}
-        </option>
-      </select>
-    </showcase-label>
-
-    <showcase-label class="col-md-6" label="Адрес">
-      <input
-        class="form-control"
-        type="text"
-        v-model="dataToSend.address"
-        placeholder="Адрес"
-      >
-    </showcase-label>
-
-    <showcase-label class="col-md-6" label="Номер ТТН">
-      <input
-        class="form-control"
-        type="text"
-        v-model="dataToSend.ttn"
-        placeholder="Номер ТТН"
-      >
-    </showcase-label>
-
-    <showcase-label class="col-md-6" label="Стоимость заказа">
-      <input
-        class="form-control"
-        type="text"
-        v-model="dataToSend.orderCost"
-        placeholder="Стоимость"
-      >
-    </showcase-label>
-
-    <showcase-label class="col-md-6" label="Номер платежного документа">
-      <input
-        class="form-control"
-        type="text"
-        v-model="dataToSend.paymentDoc"
-        placeholder="Номер"
-      >
-    </showcase-label>
-
-    <showcase-label class="col-md-4" label="Дата заказа">
-      <input
-        class="form-control"
-        type="date"
-        v-model="dataToSend.orderDate"
-      >
-    </showcase-label>
-
-    <showcase-label class="col-md-4" label="Дата отгрузки">
-      <input
-        class="form-control"
-        type="date"
-        v-model="dataToSend.shippingDate"
-      >
-    </showcase-label>
-
-    <showcase-label class="col-md-4" label="Дата доставки">
-      <input
-        class="form-control"
-        type="date"
-        v-model="dataToSend.deliveryDate"
-      >
-    </showcase-label>
-
-    <showcase-label class="col-md-6" label="Статус">
-      <select class="form-select" v-model="dataToSend.status">
-        <option disabled value="">
-          Статус
-        </option>
-        <option
-          v-for="(item, index) in status"
-          :key="index"
-          :value="item.value"
-        >
-          {{ item.text }}
-        </option>
-      </select>
-    </showcase-label>
-
-    <showcase-label class="col-md-auto mt-auto" label="Принято ТК">
-      <input
-        class="form-check-input"
-        type="checkbox"
-        v-model="dataToSend.acceptTc"
-      >
-    </showcase-label>
-
-    <showcase-label class="col-md-3 mt-auto" label="Принято клиентом">
-      <input
-        class="form-check-input"
-        type="checkbox"
-        v-model="dataToSend.acceptClient"
-      >
-    </showcase-label>
-
-    <showcase-label class="col-md-12" label="Примечание">
-      <textarea
-        class="form-control"
-        v-model="dataToSend.extra"
-      ></textarea>
-    </showcase-label>
-
-    <showcase-label class="col-md-6" label="Артикул товара">
-      <input
-        class="form-control"
-        type="text"
-        v-model="dataToSend.art"
-        placeholder="Стоимость"
-      >
-    </showcase-label>
-
-    <showcase-label class="col-md-6" label="Кол-во">
-      <input
-        class="form-control"
-        type="number"
-        v-model="dataToSend.count"
-      >
-    </showcase-label>
-
-    <showcase-submit>
-      Добавить ещё один товар
-    </showcase-submit>
-
-    <showcase-errors-list
-      v-if="v$.$errors.length"
-      :errors="v$.$errors"
-    />
-
-    <showcase-submit @click="sendToServer">
-      <edit-icon />
-      Создать
-    </showcase-submit>
-  </showcase-create>
+  <showcase-create v-bind="createData" />
 </template>
 
 <script>
-import { useVuelidate } from '@vuelidate/core'
-import ShowcaseErrorsList from '@/components/Showcase/showcase-errors-list'
 import ShowcaseCreate from '@/components/Showcase/showcase-create'
-import ShowcaseLabel from '@/components/Showcase/showcase-label-field'
-import ShowcaseSubmit from '@/components/Showcase/showcase-submit-button'
-import { EditIcon } from '@iconicicons/vue3'
-import {
-  required
-} from '@/utils/validation/i18n-validators'
+import { required } from '@/utils/validation/i18n-validators'
 
 export default {
   name: 'OrderCreate',
   components: {
-    ShowcaseCreate,
-    ShowcaseLabel,
-    ShowcaseSubmit,
-    ShowcaseErrorsList,
-    EditIcon
-  },
-  setup () {
-    return { v$: useVuelidate() }
-  },
-  validations () {
-    return {
-      dataToSend: {
-        client: { required },
-        tc: { required },
-        address: { required },
-        ttn: { required },
-        orderCost: { required },
-        paymentDoc: { required },
-        orderDate: { required },
-        shippingDate: { required },
-        deliveryDate: { required },
-        art: { required },
-        count: { required }
-      }
-    }
+    ShowcaseCreate
   },
   data: () => {
     return {
       createData: {
-        title: 'Создать заказ'
-      },
-      status: [
-        {
-          text: 'К отправке',
-          value: 1
-        }
-      ],
-      clientList: [
-        {
-          text: 'МетталПром',
-          value: 1
-        }
-      ],
-      tcList: [
-        {
-          text: '',
-          value: 1
-        }
-      ],
-      dataToSend: {
-        client: '',
-        tc: '',
-        address: '',
-        ttn: '',
-        orderCost: '',
-        paymentDoc: '',
-        orderDate: '',
-        shippingDate: '',
-        deliveryDate: '',
-        acceptTc: false,
-        acceptClient: false,
-        extra: '',
-        art: '',
-        count: ''
-      }
-    }
-  },
-  methods: {
-    async sendToServer () {
-      const isReady = await this.v$.$validate()
-
-      if (!isReady) {
-        console.log('Ошибка!')
-      } else {
-        console.log('Объект готов к отправке', this.dataToSend)
+        title: 'Создать заказ',
+        fields: [
+          {
+            label: 'Клиент',
+            type: 'select',
+            modelValue: 'client',
+            validation: { required },
+            values: [
+              {
+                text: 'Максим',
+                value: 1
+              }
+            ]
+          },
+          {
+            label: 'ТК',
+            type: 'select',
+            modelValue: 'tc',
+            validation: { required },
+            values: [
+              {
+                text: 'Максим',
+                value: 1
+              }
+            ]
+          },
+          {
+            label: 'Адрес',
+            modelValue: 'address',
+            validation: { required },
+            placeholder: 'Адрес'
+          },
+          {
+            label: 'Номер ТТН',
+            modelValue: 'ttn',
+            validation: { required },
+            placeholder: 'ТТН'
+          },
+          {
+            label: 'Стоимость заказа',
+            modelValue: 'cost',
+            validation: { required },
+            placeholder: 'Стоимость'
+          },
+          {
+            label: 'Стоимость заказа',
+            modelValue: 'orderCost',
+            validation: { required },
+            placeholder: 'Стоимость'
+          },
+          {
+            label: 'Номер платежного документа',
+            modelValue: 'paymentDoc',
+            validation: { required },
+            placeholder: 'Номер'
+          },
+          {
+            label: 'Дата заказа',
+            modelValue: 'orderDate',
+            validation: { required },
+            type: 'date'
+          },
+          {
+            label: 'Дата отгрузки',
+            modelValue: 'shippingDate',
+            validation: { required },
+            type: 'date'
+          },
+          {
+            label: 'Дата доставки',
+            modelValue: 'deliveryDate',
+            validation: { required },
+            type: 'date'
+          },
+          {
+            label: 'Статус',
+            modelValue: 'status',
+            type: 'select',
+            validation: { required },
+            values: [
+              {
+                text: 'В доставке',
+                value: 1
+              }
+            ]
+          },
+          {
+            label: 'Принято ТК',
+            modelValue: 'acceptTc',
+            type: 'checkbox',
+            cols: ['col-auto']
+          },
+          {
+            label: 'Принято клиентом',
+            modelValue: 'acceptClient',
+            type: 'checkbox',
+            cols: ['col-auto']
+          },
+          {
+            label: 'Примечание',
+            placeholder: 'Примечание',
+            modelValue: 'extra',
+            type: 'textarea',
+            cols: ['col-md-12']
+          }
+        ],
+        enableAddedFields: true,
+        subtitle: 'Товары в заказе',
+        addedFields: [
+          {
+            label: 'Артикул',
+            placeholder: 'Артикул',
+            modelValue: 'art'
+          },
+          {
+            label: 'Количество',
+            placeholder: 'Кол-во',
+            type: 'number',
+            modelValue: 'count'
+          }
+        ]
       }
     }
   }
