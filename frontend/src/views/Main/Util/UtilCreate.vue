a210bc81162973aa70c136c0a75a4af1c5b4febe
Vadim Ignatov <ignatov0131@gmail.com>
2021-12-17 21:37:16 +0300

    Rework util create

--- frontend/src/views/Main/Util/UtilCreate.vue
+++ frontend/src/views/Main/Util/UtilCreate.vue
@@ -1,191 +1,97 @@
 <template>
-  <showcase-create v-bind="createData">
-    <showcase-label class="col-md-6" label="Номер ТТН">
-      <input
-        class="form-control"
-        type="text"
-        v-model="dataToSend.ttnNumber"
-        placeholder="Номер ТТН"
-      >
-    </showcase-label>
-
-    <showcase-label class="col-md-6" label="Утилизатор">
-      <select class="form-select" v-model="dataToSend.utilizator">
-        <option disabled value="">
-          Утилизатор
-        </option>
-        <option
-          v-for="(item, index) in utilizatorList"
-          :key="index"
-          :value="item.value"
-        >
-          {{ item.text }}
-        </option>
-      </select>
-    </showcase-label>
-
-    <showcase-label class="col-md-6" label="Номер акта утилизации">
-      <input
-        class="form-control"
-        type="text"
-        v-model="dataToSend.actOfUtil"
-        placeholder="Номер акта утилизации"
-      >
-    </showcase-label>
-
-    <showcase-label class="col-md-6" label="Дата отгрузки">
-      <input
-        class="form-control"
-        type="date"
-        v-model="dataToSend.shippingDate"
-        placeholder="01.01.1990"
-      >
-    </showcase-label>
-
-    <showcase-label class="col-md-6" label="Сотрудник">
-      <select class="form-select" v-model="dataToSend.employee">
-        <option disabled value="">
-          Сотрудник
-        </option>
-        <option
-          v-for="(item, index) in employeeList"
-          :key="index"
-          :value="item.value"
-        >
-          {{ item.text }}
-        </option>
-      </select>
-    </showcase-label>
-
-    <showcase-label class="col-md-6" label="Кол-во">
-      <input
-        class="form-control"
-        type="number"
-        v-model="dataToSend.count"
-        placeholder="Кол-во"
-      >
-    </showcase-label>
-
-    <showcase-label class="col-md-12" label="Причина">
-      <input
-        class="form-control"
-        type="text"
-        v-model="dataToSend.reason"
-        placeholder="Причина"
-      >
-    </showcase-label>
-
-    <showcase-label class="col-md-6" label="Артикул товара">
-      <input
-        class="form-control"
-        type="text"
-        v-model="dataToSend.art"
-        placeholder="Артикул"
-      >
-    </showcase-label>
-
-    <showcase-label class="col-md-6" label="Кол-во">
-      <input
-        class="form-control"
-        type="number"
-        v-model="dataToSend.count"
-      >
-    </showcase-label>
-
-    <showcase-submit>
-      Добавить ещё один товар
-    </showcase-submit>
-
-    <showcase-errors-list
-      v-if="v$.$errors.length"
-      :errors="v$.$errors"
-    />
-
-    <showcase-submit @click="sendToServer">
-      <edit-icon />
-      Создать
-    </showcase-submit>
-  </showcase-create>
+  <showcase-create v-bind="createData" />
 </template>
 
 <script>
-import { useVuelidate } from '@vuelidate/core'
-import ShowcaseErrorsList from '@/components/Showcase/showcase-errors-list'
 import ShowcaseCreate from '@/components/Showcase/showcase-create'
-import ShowcaseLabel from '@/components/Showcase/showcase-label-field'
-import ShowcaseSubmit from '@/components/Showcase/showcase-submit-button'
-import { EditIcon } from '@iconicicons/vue3'
-import {
-  required
-} from '@/utils/validation/i18n-validators'
+import { required } from '@/utils/validation/i18n-validators'
 
 export default {
   name: 'AgentCreate',
   components: {
-    ShowcaseCreate,
-    ShowcaseLabel,
-    ShowcaseSubmit,
-    ShowcaseErrorsList,
-    EditIcon
-  },
-  setup () {
-    return { v$: useVuelidate() }
-  },
-  validations () {
-    return {
-      dataToSend: {
-        ttnNumber: { required },
-        utilizator: { required },
-        actOfUtil: { required },
-        shippingDate: { required },
-        employee: { required },
-        art: { required },
-        reason: { required },
-        count: { required }
-      }
-    }
+    ShowcaseCreate
   },
   data: () => {
     return {
       createData: {
-        title: 'Создать утилизацию'
-      },
-      employeeList: [
-        {
-          text: 'Птушкин Александр',
-          value: 1
-        }
-      ],
-      utilizatorList: [
-        {
-          text: 'УтилПро',
-          value: 1
-        },
-        {
-          text: 'УтилМакс',
-          value: 2
-        }
-      ],
-      dataToSend: {
-        ttnNumber: '',
-        utilizator: '',
-        actOfUtil: '',
-        shippingDate: '',
-        employee: '',
-        art: '',
-        reason: '',
-        count: ''
-      }
-    }
-  },
-  methods: {
-    async sendToServer () {
-      const isReady = await this.v$.$validate()
-
-      if (!isReady) {
-        console.log('Ошибка!')
-      } else {
-        console.log('Объект готов к отправке', this.dataToSend)
+        title: 'Создать утилизацию',
+        fields: [
+          {
+            label: 'Номер ТТН',
+            placeholder: 'ТТН',
+            modelValue: 'ttnNumber',
+            cols: ['col-md-4'],
+            validation: { required }
+          },
+          {
+            label: 'Утилизатор',
+            placeholder: 'Утилизатор',
+            modelValue: 'utilizator',
+            cols: ['col-md-4'],
+            type: 'select',
+            validation: { required },
+            values: [
+              {
+                text: 'УтилПро',
+                value: 1
+              },
+              {
+                text: 'УтилМакс',
+                value: 2
+              }
+            ]
+          },
+          {
+            label: 'Номер акта утилизации',
+            placeholder: '№',
+            cols: ['col-md-4'],
+            modelValue: 'actOfUtil',
+            validation: { required }
+          },
+          {
+            label: 'Дата отгрузки',
+            placeholder: 'Дата отгрузки',
+            modelValue: 'shippingDate',
+            type: 'date',
+            validation: { required }
+          },
+          {
+            label: 'Сотрудник',
+            placeholder: 'Сотрудник',
+            modelValue: 'employee',
+            type: 'select',
+            validation: { required },
+            values: [
+              {
+                text: 'Птушкин Александр',
+                value: 1
+              }
+            ]
+          }
+        ],
+        enableAddedFields: true,
+        subtitle: 'Товары к утилизации',
+        addedFields: [
+          {
+            label: 'Причина',
+            placeholder: 'Причина',
+            modelValue: 'reason',
+            cols: ['col-md-6']
+          },
+          {
+            label: 'Артикул товара',
+            placeholder: 'Артикул',
+            modelValue: 'art',
+            cols: ['col-md-3']
+          },
+          {
+            label: 'Кол-во',
+            placeholder: '',
+            type: 'number',
+            modelValue: 'count',
+            cols: ['col-md-3']
+          }
+        ]
       }
     }
   }
