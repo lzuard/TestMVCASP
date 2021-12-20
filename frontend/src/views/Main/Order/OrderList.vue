5a90d7aa2cced560e3c28696b39d73b6458c56b4
Vadim Ignatov <ignatov0131@gmail.com>
2021-12-20 23:33:59 +0300

    Upd order list

--- frontend/src/views/Main/Order/OrderList.vue
+++ frontend/src/views/Main/Order/OrderList.vue
@@ -4,11 +4,18 @@
 
 <script>
 import Showcase from '@/components/Showcase'
+import { useToast } from 'vue-toastification'
+import dayjs from 'dayjs'
+import { STATUSES } from '@/services/dictionaries'
 
 export default {
   name: 'OrderList',
   components: {
     Showcase
+  },
+  setup () {
+    const toast = useToast()
+    return { toast }
   },
   data: () => {
     return {
@@ -21,7 +28,7 @@ export default {
           'Клиент',
           'Сотрудник',
           'ТК',
-          'Номер адреса',
+          'Адрес',
           'ТТН',
           'Платежный документ',
           'Дата заказа',
@@ -32,41 +39,35 @@ export default {
           'Принят клиентом',
           'Примечание'
         ],
-        tableData: [
-          {
-            id: '7823',
-            client: 'ООО "Построй-ка',
-            employee: 'Иванов И.И.',
-            tc: 'Паровёз',
-            address: '1732',
-            ttn: '29873/НУ1',
-            payment_doc: '597824',
-            order_date: '27.06.2021',
-            shipment_date: '29.06.2021',
-            delivery_date: '01.07.2021',
-            status: 'Завершен',
-            accept_tc: 'Да',
-            accept_client: 'Да',
-            extra: ''
-          }
-        ],
-        sortTypes: [
-          'Номер заказа',
-          'Клиент',
-          'Сотрудник',
-          'ТК',
-          'Номер адреса',
-          'ТТН',
-          'Платежный документ',
-          'Дата заказа',
-          'Дата отгрузки',
-          'Дата доставки',
-          'Статус',
-          'Принят ТК',
-          'Принят клиентом',
-          'Примечание'
-        ]
+        tableData: []
       }
+    }
+  },
+  created () {
+    this.$api.order.getOrders()
+      .then((data) => {
+        this.handlingOrders(data)
+        this.showCaseData.tableData = data
+      })
+      .catch(() => {
+        this.toast.error('Ошибка при загрузке заказов')
+      })
+  },
+  methods: {
+    handlingOrders (data) {
+      data.forEach((item) => {
+        item.client = item.client.organizationName
+        item.employee = `${item.employee.secondName} ${item.employee.firstName}`
+        item.transportCompany = item.transportCompany.organizationName
+        item.address = `${item.address.index}, ${item.address.subject}, ${item.address.location}, ${item.address.street ?? '-'}`
+        item.ttn = item.ttn.number
+        item.orderingDate = dayjs(item.orderingDate).locale('ru-ru').format('DD/MM/YYYY')
+        item.shipmentDate = dayjs(item.shipmentDate).locale('ru-ru').format('DD/MM/YYYY')
+        item.deliveryDate = dayjs(item.deliveryDate).locale('ru-ru').format('DD/MM/YYYY')
+        item.isAcceptClient = item.isAcceptClient ? 'Да' : 'Нет'
+        item.isAcceptTransportCompany = item.isAcceptTransportCompany ? 'Да' : 'Нет'
+        item.status = STATUSES[item.status]
+      })
     }
   }
 }
