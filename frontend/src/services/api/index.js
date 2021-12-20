9b5f0b823d8485aa85e326f37d7945dffe3a8acd
Vadim Ignatov <ignatov0131@gmail.com>
2021-12-20 16:53:30 +0300

    Upd api

--- frontend/src/services/api/index.js
+++ frontend/src/services/api/index.js
@@ -4,35 +4,41 @@ export const authorizeUser = (payload) => {
   return api.post('/Employee/auth', payload).then(({ data }) => data)
 }
 
-const getAgents = (payload) => {
-  return api.get('/agents', payload).then(({ data }) => {
-    console.log(data)
-  })
+const createAddress = (payload) => {
+  return api.post('/Address', payload).then(({ data }) => data)
 }
 
-const createAgent = (payload) => {
-  return api.post('/agents', payload).then(({ data }) => {
-    console.log(data)
-  })
+const getAddress = () => {
+  return api.get('/Address').then(({ data }) => serialize(data))
 }
 
-const getTTN = (payload) => {
-  return api.get('/ttn', payload).then(({ data }) => {
-    console.log(data)
-  })
+const getTTN = () => {
+  return api.get('/Ttn').then(({ data }) => serialize(data))
 }
 
 const createTTN = (payload) => {
-  return api.post('/ttn', payload).then(({ data }) => {
-    console.log(data)
+  return api.post('/Ttn', payload).then(({ data }) => data)
+}
+
+const serialize = (payload) => {
+  return payload.map((item, index) => {
+    delete item.id
+
+    for (const key in item) {
+      if (!item[key]) {
+        item[key] = '-'
+      }
+    }
+
+    return Object.assign({ id: index + 1 }, { ...item })
   })
 }
 
 export default {
   authorizeUser,
-  agents: {
-    getAgents,
-    createAgent
+  address: {
+    createAddress,
+    getAddress
   },
   ttn: {
     getTTN,
