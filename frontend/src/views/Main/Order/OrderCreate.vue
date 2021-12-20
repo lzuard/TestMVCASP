a0a0130338ad92ff5ca8a2590c86d1d5bc89a515
Vadim Ignatov <ignatov0131@gmail.com>
2021-12-20 23:40:35 +0300

    Upd order create

--- frontend/src/views/Main/Order/OrderCreate.vue
+++ frontend/src/views/Main/Order/OrderCreate.vue
@@ -1,15 +1,24 @@
 <template>
-  <showcase-create v-bind="createData" />
+  <showcase-create
+    ref="form"
+    v-bind="createData"
+    @onSend="createOrder"
+  />
 </template>
 
 <script>
 import ShowcaseCreate from '@/components/Showcase/showcase-create'
 import { required } from '@/utils/validation/i18n-validators'
+import { useToast } from 'vue-toastification'
 
 export default {
   name: 'OrderCreate',
   components: {
     ShowcaseCreate
+  },
+  setup () {
+    const toast = useToast()
+    return { toast }
   },
   data: () => {
     return {
@@ -19,66 +28,55 @@ export default {
           {
             label: 'Клиент',
             type: 'select',
-            modelValue: 'client',
+            modelValue: 'clientId',
             validation: { required },
-            values: [
-              {
-                text: 'Максим',
-                value: 1
-              }
-            ]
+            values: []
+          },
+          {
+            label: 'Сотрудник',
+            type: 'select',
+            modelValue: 'employeeId',
+            validation: { required },
+            values: []
           },
           {
             label: 'ТК',
             type: 'select',
-            modelValue: 'tc',
+            modelValue: 'transportCompanyId',
             validation: { required },
-            values: [
-              {
-                text: 'Максим',
-                value: 1
-              }
-            ]
+            values: []
           },
           {
             label: 'Адрес',
-            modelValue: 'address',
+            modelValue: 'addressId',
+            placeholder: 'Адрес',
             validation: { required },
-            placeholder: 'Адрес'
+            type: 'select',
+            values: []
           },
           {
             label: 'Номер ТТН',
-            modelValue: 'ttn',
+            modelValue: 'ttnId',
             validation: { required },
-            placeholder: 'ТТН'
-          },
-          {
-            label: 'Стоимость заказа',
-            modelValue: 'cost',
-            validation: { required },
-            placeholder: 'Стоимость'
-          },
-          {
-            label: 'Стоимость заказа',
-            modelValue: 'orderCost',
-            validation: { required },
-            placeholder: 'Стоимость'
+            placeholder: 'ТТН',
+            type: 'select',
+            values: []
           },
           {
             label: 'Номер платежного документа',
-            modelValue: 'paymentDoc',
+            modelValue: 'paymentDocument',
             validation: { required },
             placeholder: 'Номер'
           },
           {
             label: 'Дата заказа',
-            modelValue: 'orderDate',
+            modelValue: 'orderingDate',
             validation: { required },
             type: 'date'
           },
           {
             label: 'Дата отгрузки',
-            modelValue: 'shippingDate',
+            modelValue: 'shipmentDate',
             validation: { required },
             type: 'date'
           },
@@ -95,20 +93,28 @@ export default {
             validation: { required },
             values: [
               {
-                text: 'В доставке',
+                text: 'Принят',
+                value: 0
+              },
+              {
+                text: 'Завершен',
                 value: 1
+              },
+              {
+                text: 'Возврат',
+                value: 2
               }
             ]
           },
           {
             label: 'Принято ТК',
-            modelValue: 'acceptTc',
+            modelValue: 'isAcceptTransportCompany',
             type: 'checkbox',
             cols: ['col-auto']
           },
           {
             label: 'Принято клиентом',
-            modelValue: 'acceptClient',
+            modelValue: 'isAcceptClient',
             type: 'checkbox',
             cols: ['col-auto']
           },
@@ -124,18 +130,95 @@ export default {
         subtitle: 'Товары в заказе',
         addedFields: [
           {
-            label: 'Артикул',
+            label: 'Товар',
             placeholder: 'Артикул',
-            modelValue: 'art'
+            type: 'select',
+            modelValue: 'productId',
+            values: []
           },
           {
             label: 'Количество',
             placeholder: 'Кол-во',
             type: 'number',
-            modelValue: 'count'
+            modelValue: 'quantity'
           }
         ]
       }
+    }
+  },
+  created () {
+    this.$api.agent.getAgentByCategory(1)
+      .then((data) => {
+        this.createData.fields.find(item => item.modelValue === 'clientId').values = data.map(item => {
+          return Object.assign({}, {
+            text: item.organizationName,
+            value: item.id
+          })
+        })
+      })
+
+    this.$api.agent.getAgentByCategory(3)
+      .then((data) => {
+        this.createData.fields.find(item => item.modelValue === 'transportCompanyId').values = data.map(item => {
+          return Object.assign({}, {
+            text: item.organizationName,
+            value: item.id
+          })
+        })
+      })
+
+    this.$api.employee.getEmployee()
+      .then((data) => {
+        this.createData.fields.find(item => item.modelValue === 'employeeId').values = data.map(item => {
+          return Object.assign({}, {
+            text: `${item.secondName} ${item.firstName} ${item.thirdName}`,
+            value: item.id
+          })
+        })
+      })
+
+    this.$api.address.getAddressForUsing()
+      .then((data) => {
+        this.createData.fields.find(item => item.modelValue === 'addressId').values = data.map(item => {
+          return Object.assign({}, {
+            text: `${item.index}, ${item.subject}, ${item.location}, ${item.street}, ${item.house}`,
+            value: item.id
+          })
+        })
+      })
+
+    this.$api.ttn.getTTNForUsing()
+      .then((data) => {
+        this.createData.fields.find(item => item.modelValue === 'ttnId').values = data.map(item => {
+          return Object.assign({}, {
+            text: item.number,
+            value: item.id
+          })
+        })
+      })
+
+    this.$api.product.getProductsForUsing()
+      .then((data) => {
+        this.createData.addedFields.find(item => item.modelValue === 'productId').values = data.map(innerItem => {
+          return {
+            text: `${innerItem.name} | Артикул: ${innerItem.art}`,
+            value: innerItem.id
+          }
+        })
+      })
+  },
+  methods: {
+    createOrder (data, additional) {
+      const newData = { ...data, orderProducts: additional }
+
+      this.$api.order.createOrder(newData)
+        .then(() => {
+          this.toast.success('Заказ успешно создан')
+          this.$refs.form.clearForm()
+        })
+        .catch(() => {
+          this.toast.error('Ошибка при создании заказа, повторите позже')
+        })
     }
   }
 }
