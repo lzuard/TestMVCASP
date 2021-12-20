c40010c69ee02911ed336b467086610b2d6b67ac
Vadim Ignatov <ignatov0131@gmail.com>
2021-12-20 19:45:48 +0300

    Upd api

--- frontend/src/services/api/index.js
+++ frontend/src/services/api/index.js
@@ -12,12 +12,48 @@ const getAddress = () => {
   return api.get('/Address').then(({ data }) => serialize(data))
 }
 
+const getAddressForUsing = () => {
+  return api.get('/Address').then(({ data }) => serialize(data))
+}
+
 const getTTN = () => {
   return api.get('/Ttn').then(({ data }) => serialize(data))
 }
 
 const createTTN = (payload) => {
   return api.post('/Ttn', payload).then(({ data }) => data)
+}
+
+const getParentsCategory = () => {
+  return api.get('/Category').then(({ data }) => data)
+}
+
+const getCategory = () => {
+  return api.get('/Category').then(({ data }) => serialize(data))
+}
+
+const createCategory = (payload) => {
+  return api.post('/Category/create', payload).then(({ data }) => data)
+}
+
+const getProducts = () => {
+  return api.get('/Product').then(({ data }) => serialize(data))
+}
+
+const createProduct = (payload) => {
+  return api.post('/Product/create', payload).then(({ data }) => data)
+}
+
+const getAgentTypes = () => {
+  return api.get('/AgentType').then(({ data }) => data)
+}
+
+const getAgents = () => {
+  return api.get('/Agent').then(({ data }) => serialize(data))
+}
+
+const createAgent = (payload) => {
+  return api.post('/Agent/create', payload).then(({ data }) => data)
 }
 
 const serialize = (payload) => {
@@ -38,10 +74,25 @@ export default {
   authorizeUser,
   address: {
     createAddress,
-    getAddress
+    getAddress,
+    getAddressForUsing
   },
   ttn: {
     getTTN,
     createTTN
+  },
+  category: {
+    getCategory,
+    getParentsCategory,
+    createCategory
+  },
+  product: {
+    getProducts,
+    createProduct
+  },
+  agent: {
+    getAgentTypes,
+    getAgents,
+    createAgent
   }
 }
